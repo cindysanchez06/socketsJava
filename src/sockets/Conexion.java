cdcc22e0aceef75752d428f011770a82db9a092e
Cindy Sanchez <cindyk_98@hotmail.com>
2021-09-21 00:40:41 -0500

    Update Conexion.java

--- src/sockets/Conexion.java
+++ src/sockets/Conexion.java
@@ -8,13 +8,12 @@ import java.net.Socket;
 public class Conexion
 {
     private final int PUERTO = 8080; //Puerto para la conexión
-    private final String HOST = "localhost"; //Host para la conexión
+    private final String HOST = "192.168.20.51"; //Host para la conexión
     protected String mensajeServidor; //Mensajes entrantes (recibidos) en el servidor
     protected ServerSocket ss; //Socket del servidor
     protected Socket cs; //Socket del cliente
     protected DataOutputStream salidaServidor, salidaCliente; //Flujo de datos de salida
 
-    
     public Conexion(String tipo) throws IOException //Constructor
     {
         if(tipo.equalsIgnoreCase("server"))
