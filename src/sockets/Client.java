96bad6b0ff04851ac891458d2cba189905441c3f
Cindy Sanchez <cindyk_98@hotmail.com>
2021-09-21 00:39:30 -0500

    Update Client.java

--- src/sockets/Client.java
+++ src/sockets/Client.java
@@ -1,6 +1,5 @@
 package sockets;
 
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 import sockets.Conexion;
@@ -19,11 +18,15 @@ public class Client extends Conexion
             //Se enviarán dos mensajes
             for (int i = 0; i < 2; i++)
             {
+   
+            	System.out.println("enviando mensaje");
                 //Se escribe en el servidor usando su flujo de datos
-                salidaServidor.writeUTF("Este es el mensaje número " + (i+1) + "\n");
+                salidaServidor.writeUTF("Este es el mensaje número " + (i+1) + " desde ip 192.168.20.52 a ip 192.168.20.51 \n");
+                //salidaServidor.flush();
+                
             }
-
-            cs.close();//Fin de la conexión
+            salidaServidor.writeUTF("stop");
+            cs.close();//Fin de la conexiónte
 
         }
         catch (Exception e)
